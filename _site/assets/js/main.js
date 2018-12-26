992b3839da925e1fa3bf203a673c04615dab1c6a
viktor2222 <44213619+viktor2222@users.noreply.github.com>
2018-12-26 14:10:14 +0200

    Update main.js

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,22 +1,3 @@
-$(window).on('load', function() {
-  setTimeout(function() {
-      if ($('.js-overlay-campaign').hasClass('disabled')) {
-          return !1
-      } else {
-          $(".js-overlay-campaign").fadeIn()
-      }
-  })
-});
-$('.js-close-campaign').click(function() {
-    $('.js-overlay-campaign').fadeOut()
-});
-$(document).mouseup(function(e) {
-    var popup = $('.js-popup-campaign');
-    if (e.target != popup[0] && popup.has(e.target).length === 0) {
-        $('.js-overlay-campaign').fadeOut()
-    }
-});
-
 $(".navbar ul li a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
